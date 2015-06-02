0571b9846f4c5faf6010e89459db8409acbe15d7
connorshea <connor.james.shea@gmail.com>
2015-06-02 17:08:54 -0600

    New better/faster/stronger front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   
-  
   resources :stories do
     resources :chapters
   end
+  
   resources :characters
   resources :franchises
   resources :users
 
   devise_for :users, path: "accounts"
-  root 'frontpage#frontpage'
+  
+  root 'frontpage#index'
+
 end
