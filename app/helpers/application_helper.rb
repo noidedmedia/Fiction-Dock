9e81417ba1bdb2cf17f1c7bd8568df1934d1ded9
connorshea <connor.james.shea@gmail.com>
2015-06-02 15:06:02 -0600

    HTML Title, Pretty Time in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def pretty_time(t)
+    t.strftime("%l:%M %p %B %d, %Y")
+  end
+
+  ##
+  # Used for page's HTML titles.
+  # http://stackoverflow.com/a/3841549
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
 end
