04c22a6c7d312d22c6ef02634fe50a62414c87d8
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-06-02 16:19:19 -0600

    stuff

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,27 @@
 class StoriesController < ApplicationController
+  include Pundit
+  before_filter :authenticate_user!, except: [:show, :index]
+  def index
+    @franchise = Franchise.find(params[:franchise_id])
+    @stories = @franchise.stories
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def new
+    @franchise = Franchise.find(params[:franchise_id])
+    @story = Story.new(user: current_user,
+                       franchises: [@franchsie])
+  end
+
+  def story_params
+    params.require(:story)
+      .permit(:name,
+              :blurb,
+              :description,
+              franchises: [:id])
+  end
+  end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -5,4 +5,12 @@ class Story < ActiveRecord::Base
   has_many :characters, through: :story_characters
   has_many :story_franchises
   has_many :franichises, through: :story_franchises
+
+  accepts_nested_attributes_for :franchies, reject_if: :new_record?
+  validate :has_at_least_one_franchise
+  protected
+  def has_at_least_one_franchise
+    errors.add(:franchises, "must have at least one") if franchises.length < 1
+  end
+
 end
