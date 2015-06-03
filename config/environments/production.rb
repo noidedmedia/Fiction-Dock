7d0fa60663e74dcde35f4a51d4450878348f6c14
connorshea <connor.james.shea@gmail.com>
2015-06-03 17:40:43 -0600

    yolo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=#{2.hours.to_i}"
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
