8615005affd1a1861a3a3f6a5e7dfca9addc5e0d
connorshea <connor.james.shea@gmail.com>
2015-06-02 23:00:23 -0600

    More good editing stuff

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -2,6 +2,11 @@ function markdownEditor() {
   var markdownelement = document.querySelector(".editor > .markdown > pre");
   new MediumEditor(document.querySelector(".editor > .rich-text"), {
     buttons: ["bold", "italic", "underline", "orderedlist", "unorderedlist", "header1", "header2", "quote"],
+    paste: {
+        cleanPastedHTML: true,
+        cleanAttrs: ['style', 'dir'],
+        cleanTags: ['label', 'meta']
+    },
     extensions: {
       markdown: new MeMarkdown(function (md) {
         markdownelement.innerText = md;
