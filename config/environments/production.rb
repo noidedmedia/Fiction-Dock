429b2818d5893124fddfd03b6f75399afc1682a3
connorshea <connor.james.shea@gmail.com>
2015-06-03 17:48:50 -0600

    1 month cache, yolo
    We’ll have to modify config.assets.version to get the browser to notice
    changes in the future

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.serve_static_files = true
-  config.static_cache_control = "public, max-age=#{12.hours.to_i}"
+  config.static_cache_control = "public, max-age=#{1.month.to_i}"
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
