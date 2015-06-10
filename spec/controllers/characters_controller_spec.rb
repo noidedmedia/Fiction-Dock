2f4fc33ddd5d1863782e9757b82d5e72b7c8082d
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-06-10 11:19:39 -0600

    typo

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CharactersController, type: :controller do
+  it do 
+    should permit(:name, :description)
+      .for(:create)
+      .on(:character)
+  end
 
+
+  
 end
