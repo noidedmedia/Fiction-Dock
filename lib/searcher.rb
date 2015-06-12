2d6a2527c82899617bebe29ac311b52d23f4ad08
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-06-11 21:26:59 -0600

    Searcher Helper Object
    This helps us find stories. It uses some kind of nasty arel (plus SQL
    strings, because Arel can't do "INNER JOIN table_name AS alias") to
    select stories based on user-given criteria.
    
    Now to plug integrate it with the controller.

--- lib/searcher.rb
+++ lib/searcher.rb
@@ -1,17 +1,67 @@
 class Searcher
-  def initialize(hsh)
-    @hash = hsh
-    @ships = @hash["ships"]
-
-    prep_ships
+  def initialize(hash)
+    @hash = hash
+    prepare_hash
   end
-
+  
   def resolve
-
+    @query = stories.project(stories[Arel.star]).group(stories[:id])
+    resolve_ship_chars if @ship_chars && @ship_chars.length > 0
+    resolve_characters if @chars && @chars.length > 0 
+    Story.find_by_sql(@query.to_sql)
   end
-
+  attr_reader :query
   protected
-  def prep_ships
-    @ships.map!{|id_list| id_list.split(",").map(&:to_i)}
+  def prepare_hash
+    load_ship
+    load_chars
   end
+
+  def load_ship
+    @ship_chars = @hash["ship"]
+    if @ship_chars
+      @ship_chars = @ship_chars.split(",").map!(&:to_i)
+    end
+  end
+
+  def load_chars
+    @chars = @hash["characters"]
+    if @chars
+      @chars = @chars.split(", ").map!(&:to_i)
+    end
+  end
+  def resolve_characters
+    @query = @query
+      .join(story_characters).on(story_characters[:story_id].eq(stories[:id]))
+      .join(characters).on(characters[:id].eq(story_characters[:character_id]))
+      .where(characters[:id].in(@chars))
+      .having(Arel.sql("COUNT(DISTINCT characters)").eq(@chars.length))
+  end
+  def resolve_ship_chars
+    @query = @query
+      .join(ships).on(ships[:story_id].eq(stories[:id]))
+      .join(ship_characters).on(ship_characters[:ship_id].eq(ships[:id]))
+      .join(Arel.sql("INNER JOIN characters as shipchars on shipchars.id = ship_characters.character_id"))
+      .where(Arel.sql("shipchars.id").in(@ship_chars))
+      .having(Arel.sql("COUNT(DISTINCT shipchars)").eq(@ship_chars.count))
+  end
+  private
+  def ship_characters
+    @_ship_charactrs ||= ShipCharacter.arel_table
+  end
+  def story_characters
+    @_story_characters ||= StoryCharacter.arel_table
+  end
+  def stories
+    @_stories ||= Story.arel_table
+  end
+
+  def ships
+    @_ships ||= Ship.arel_table
+  end
+
+  def characters
+    @_characters ||= Character.arel_table
+  end
+
 end
--- spec/lib/searcher_spec.rb
+++ spec/lib/searcher_spec.rb
@@ -1,14 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Searcher do
+  let(:characters){3.times.map{FactoryGirl.create(:character)}}
+  let(:story_with_neither){ FactoryGirl.create(:story)}
+  let(:story_with_characters){FactoryGirl.create(:story,
+                                                 characters: characters)}
+  let(:story_with_ship){FactoryGirl.create(:story,
+                                           characters: characters)}
+  let(:ship){FactoryGirl.create(:ship,
+                                story: story_with_ship)}
   it "finds via ships" do
-    desired = FactoryGirl.create(:story)
-    not_desired = FactoryGirl.create(:story)
-    ship = FactoryGirl.create(:ship, story: desired)
-    hs = { "ships" => [
-      ship.characters.pluck(:id).join(", ")
-    ]}
+    hs = {
+      "ship" => ship.characters.pluck(:id).join(", ")
+    }
     searcher = Searcher.new(hs)
-    expect(searcher.resolve).to eq([desired])
+    expect(searcher.resolve).to eq([story_with_ship])
+  end
+  it "finds via characters" do
+    hs = { 
+      "characters" => characters.map(&:id).join(", ")
+    }
+    characters.each do |car|
+      story_with_ship.characters << car
+      story_with_characters.characters << car
+    end
+    searcher = Searcher.new(hs)
+    expect(searcher.resolve).to contain_exactly(story_with_characters, 
+                                                story_with_ship)
+
   end
 end
