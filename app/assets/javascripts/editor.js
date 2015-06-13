8b6f67f7538f9be732ce73f43bb7a23741862aa4
connorshea <connor.james.shea@gmail.com>
2015-06-12 20:24:39 -0600

    Displays alert if changes have been made and the user attempts to navigate to another page without saving.
    Useful! Doesn’t work with in-site pages, however in that case, because
    of Turbolinks, just pressing “back” will bring you back to the page
    with all the changes you made.
    
    So that’s nifty.

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -2,7 +2,7 @@ function markdownEditor() {
   var markdownelement = document.querySelector(".editor > #markdown");
   var placeholder = $("#rich-text").data("placeholder");
   new MediumEditor(document.querySelector(".editor > #rich-text"), {
-    buttons: ["bold", "italic", "underline", "orderedlist", "unorderedlist", "header1", "header2", "quote"],
+    buttons: ["bold", "italic", "orderedlist", "unorderedlist", "header1", "header2", "quote"],
     firstHeader: 'h4',
     secondHeader: 'h5',
     paste: {
@@ -23,9 +23,39 @@ function markdownEditor() {
   });
 }
 
+function preventUnloadIfChanged() {
+  $("#rich-text").bind('input', function() {
+    // Unbind it so it stops listening, we only need to detect one input to
+    // determine that the text has been changed.
+    $("#rich-text").unbind('input');
+
+    // Declare "submitted" variable
+    // Used to determine if the form is being submitted or not.
+    var submitted = false;
+
+    // ".onbeforeunload" is a function for when the page
+    window.onbeforeunload = function() {
+      // If the form is being submitted, the alert shouldn't be displayed.
+      // Otherwise, display it.
+      if (!submitted) {
+        var alertmsg = $("#rich-text").data("onbeforeunload");
+        return alertmsg;
+      }
+    }
+
+    // When the form is submitted, the "submitted" variable is
+    // changed to true to ensure that the alert message won't
+    // be shown.
+    $("form").submit(function() {
+      submitted = true;
+    });
+  });
+}
+
 // Functions run when the document is "ready".
 var ready = function() {
   markdownEditor();
+  preventUnloadIfChanged();
 };
 
 $(document).ready(ready);
