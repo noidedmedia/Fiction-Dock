e2e5d1eb5dc042067d413c91e13a4f625e130fa2
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-06-16 21:48:30 -0600

    Fix franchise name length requirement

--- app/models/franchise.rb
+++ app/models/franchise.rb
@@ -7,7 +7,7 @@
 #           that this does include crossovers.
 # characters:: characters that belong to this franchise.
 class Franchise < ActiveRecord::Base
-  validates :name, length: {minimum: 5, maximum: 100}
+  validates :name, length: {minimum: 2, maximum: 100}
   validates :description, length: {minimum: 25, maximum: 1000}
   extend FriendlyId
   friendly_id :name, use: :slugged
