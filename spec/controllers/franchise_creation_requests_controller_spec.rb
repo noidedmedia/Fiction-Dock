e85dfad157daeaa03938d2050f20f2efcb1e6a19
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-06-27 18:54:15 -0600

    FranchiseCreationRequest
    Allows a user to request the creation of a franchise.

---
+++ spec/controllers/franchise_creation_requests_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe FranchiseCreationRequestsController, type: :controller do
+
+end
---
+++ spec/factories/franchise_creation_requests.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :franchise_creation_request do
+    user nil
+name "MyString"
+description "MyText"
+reason "MyText"
+  end
+
+end
