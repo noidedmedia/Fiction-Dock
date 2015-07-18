62bf12c66fcdb892468d3826716914035264b2ae
connorshea <connor.james.shea@gmail.com>
2015-07-18 16:37:58 -0600

    Error handling but not bookshelves!

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -47,13 +47,10 @@ class CharactersController < ApplicationController
     respond_to do |format|
       if @character.save
         format.html { redirect_to [@franchise, @character] }
-        format.json { render 'show' }
+        format.json { render :show }
       else
-        format.html { render 'new' }
+        format.html { redirect_to :back, warning: @character.errors.full_messages.join(", ") }
         format.json { render json: @character.errors, status: :unprocessable_entity }
-        @character.errors.full_messages.each do |message|
-          flash[:warning] = message
-        end
       end
     end
   end
@@ -67,13 +64,10 @@ class CharactersController < ApplicationController
     respond_to do |format|
       if @character.update(character_params)
         format.html { redirect_to [@franchise, @character] }
-        format.json { render 'show' }
+        format.json { render :show }
       else
-        format.html { render 'new' }
+        format.html { redirect_to :back, warning: @character.errors.full_messages.join(", ") }
         format.json { render json: @character.errors, status: :unprocessable_entity }
-        @character.errors.full_messages.each do |message|
-          flash[:warning] = message
-        end
       end
     end
   end
--- app/controllers/franchises_controller.rb
+++ app/controllers/franchises_controller.rb
@@ -62,7 +62,7 @@ class FranchisesController < ApplicationController
         format.html { redirect_to @franchise, notice: I18n.t(".notices.franchise_updated_successfully") }
         format.json { render :show, status: :created, location: @franchise}
       else
-        format.json { render :edit }
+        format.json { redirect_to :back, warning: @franchise.errors.full_messages.join(", ") }
         format.json { render json: @franchise.errors, status: :unprocessable_entity }
       end
     end
@@ -79,7 +79,7 @@ class FranchisesController < ApplicationController
         format.html { redirect_to @franchise, notice: I18n.t(".notices.franchise_created_successfully") }
         format.json { render :show, status: :created, location: @franchise }
       else
-        format.html { render :new }
+        format.html { redirect_to :back, warning: @franchise.errors.full_messages.join(", ") }
         format.json { render json: @franchise.errors, status: :unprocessable_entity }
       end
     end
