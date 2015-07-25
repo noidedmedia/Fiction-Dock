b0422902c1c49e8e85d720ceeda0459dabe797c6
connorshea <connor.james.shea@gmail.com>
2015-07-25 15:32:33 -0600

    Refactoring this didn't fix the problem, but it probably works better!
    Probably.

--- app/assets/javascripts/components/suggestions.js.jsx
+++ app/assets/javascripts/components/suggestions.js.jsx
@@ -2,15 +2,20 @@
 /* Suggestions
    ============================================== */
 
+var SuggestionsListItem = React.createClass({
+  render: function() {
+    return <li onClick={this.props.onClick}>{this.props.data.name}</li>;
+  }
+});
+
 var Suggestions = React.createClass({
   render: function() {
     return (
       <div className={ this.props.showsuggestions ? "suggestions-container active" : "suggestions-container inactive"}>
         <ul className="suggestions">
           {this.props.suggestions.map(function(item, i) {
-            return (
-              <li key={this.props.itemtype + '-' + item.id} data={item} onClick={ this.props.bindnull ? this.props.itemOnClick.bind(null, item) : this.props.itemOnClick }>{item.name}</li>
-            );
+            var itemtype = this.props.itemtype;
+            return <SuggestionsListItem key={itemtype + '-' + item.id} data={item} onClick={ this.props.bindnull ? this.props.itemOnClick.bind(null, item) : this.props.itemOnClick } />;
           }, this)}
         </ul>
       </div>
