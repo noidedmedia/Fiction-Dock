936342d30ee7a957b1dd7a3d90692122e68850a1
connorshea <connor.james.shea@gmail.com>
2015-08-21 17:27:07 -0600

    Initial commit of "View more information..."

--- app/assets/javascripts/components/bookshelf_display.js.jsx
+++ app/assets/javascripts/components/bookshelf_display.js.jsx
@@ -1,19 +1,30 @@
-var BookshelfList = React.createClass({
-  render: function(){
+var ViewMoreInformation = React.createClass({
+  render: function() {
     return (
-      <ul className="bookshelf-display-list">
-        {this.props.bookshelves.map(function(b){
-          return <BookshelfItem {...b} key={b.id}  />;
-        })}
-      </ul>);
+      <div className="more-information">
+        <div className="view-more-information-button">{this.props.translations.view_more_information}</div>
+      </div>
+    );
   }
 });
-var BookshelfItem = React.createClass({
-  render: function(){
+
+var BookshelfList = React.createClass({
+  render: function() {
+    return (
+      <ul className="bookshelf-display-list">
+        {this.props.bookshelves.map(function(b) {
+          return <BookshelfListItem {...b} key={b.id}  />;
+        })}
+      </ul>
+    );
+  }
+});
+
+var BookshelfListItem = React.createClass({
+  render: function() {
     return (
       <li>
-        <a className="bookshelf-name" 
-          href={this.props.url}>
+        <a className="bookshelf-name" href={this.props.url}>
           {this.props.name}
         </a>
       </li>
