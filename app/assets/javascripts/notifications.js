69e229e8eff5f7fcb75ba7f864ac7d62abb2e172
connorshea <connor.james.shea@gmail.com>
2015-09-01 16:24:40 -0600

    Commented_on is weird
    Clicking on a notification marks it as read then links to its subject,
    so that’s cool.

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,13 +1,21 @@
-$(function(){
-  $(".notification-item").click(function(event){
-    console.log("marking #" + this.dataset.id + "as read");
+$(function() {
+  // Links inside the Notification item will not redirect when clicked.
+  $(".notification-item a").click(function(e) {
+    e.stopPropagation();
+  });
+
+  // Marks the notification as read and redirects to the subject path.
+  $(".notification-item").click(function(event) {
+    console.log("Marking #" + this.dataset.id + " as read");
     $.ajax({
       type: "POST",
-      url: "/notifications/" + this.dataset.id + "/read", 
-      success: function(){
-        console.log("Marked as read");
-      },
-      dataType: "json"
+      dataType: "json",
+      url: "/notifications/" + this.dataset.id + "/read",
+      success: function() {
+        // On success, redirect to the subject path.
+        var path = this.dataset.subjectpath;
+        Turbolinks.visit(path);
+      }.bind(this)
     });
   })
-})
+});
