3f2eec835c22fb1c156bced212d16bf58c8e6e6c
connorshea <connor.james.shea@gmail.com>
2015-09-18 17:18:29 -0600

    Spacin'

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -9,42 +9,50 @@ class ApplicationPolicy
     attr_accessor :user
     attr_accessor :scope
   end
+  
   ##
   # Default initializer
   def initialize(user, record)
     @user = user
     @record = record
   end
+
   ##
   # Indexes are public by default
   def index?
     true
   end
+
   ##
   # by default, you can view
   def show?
     true
   end
+
   ##
   # By default, disallow creation
   def create?
     false
   end
+
   ##
   # Only allow new if we can create
   def new?
     create?
   end
+
   ##
   # BY default, disallow udpate
   def update?
     false
   end
+
   ##
   # Only allow edit if we can update
   def edit?
     update?
   end
+
   ## 
   # By default, disallow destroy
   def destroy?
